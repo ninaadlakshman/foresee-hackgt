c6ecb24b8823cc3da7f23c5c310e6a2153c90259
ninaadlakshman <ninaadlak@gmail.com>
2021-10-23 16:35:29 -0400

    Makes store service.

--- src/app/storelist/storelist.component.ts
+++ src/app/storelist/storelist.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from 'src/interfaces/store.class';
 import { ApiService } from 'src/services/api.service';
+import { StoreService } from 'src/services/store.service';
 
 @Component({
   selector: 'app-storelist',
@@ -12,11 +13,12 @@ export class StorelistComponent implements OnInit {
   stores: Store[] = [];
   constructor(
     private readonly apiService: ApiService,
+    private readonly storeService: StoreService,
     private route: ActivatedRoute,
     private router: Router ) { }
 
   ngOnInit(): void {
-    this.apiService.getStores().subscribe((stores: Store[]) => {
+    this.storeService.getStores().subscribe((stores: Store[]) => {
       this.stores = stores;
     })
   }
