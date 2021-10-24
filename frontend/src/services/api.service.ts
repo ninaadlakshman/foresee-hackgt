07510654e90153e6143bdcb1f357ff8d23ef44ee
ninaadlakshman <ninaadlak@gmail.com>
2021-10-24 08:27:32 -0400

    Made some changes to the wording on the website.

--- frontend/src/services/api.service.ts
+++ frontend/src/services/api.service.ts
@@ -12,8 +12,8 @@ export class ApiService {
   url = "https://api.ncr.com/site/sites/find-nearby/";
   constructor(private readonly http: HttpClient) { }
   getFootTraffic(latitude: string, longitude: string): Observable<string> {
-    const val = Math.floor(Math.random() * 4700) + 1700;
-    const val2 = Math.floor(Math.random() * 2500) + 1000;
+    const val = Math.floor(Math.random() * 300) + 100;
+    const val2 = Math.floor(Math.random() * 2500) + 800;
     return of(val.toString()).pipe(delay(val2));
   }
 }
